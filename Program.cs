ceb9e1deed91721a47905808311f75c3ec17b2c1
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-01-30 17:23:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
